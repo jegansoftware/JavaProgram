e8c09e25ae9de32164f7a4794a8815467cded3cb
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-05 17:37:18 +0530

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,24 @@
+//This program should be considered as Main Program
+
+package Java;
+
+public class Java {
+
+	public static void main(String[] args) {
+		JavaProgram in=new JavaProgram();
+		 Java2 k=new Java2();
+		in.eat(3,20);
+		k.logic();
+		
+		
+		Remainder remain=new Remainder();
+		
+		remain.remainder();
+		
+		Rateofinterest prt=new Rateofinterest();
+		
+		prt.main();
+		
+	}
+
+}
